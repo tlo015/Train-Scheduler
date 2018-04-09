cde16f0523432013be1fedf92a705c8c14a5aef1
Regina Lo <35210714+tlo015@users.noreply.github.com>
2018-04-09 00:14:54 -0700

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -12,11 +12,15 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
+var name;
+var destination;
+var time;
+var frequency;
 
 //on click set the data to the firebase 
 $("#submit_button").on("click", function() {
- 
+event.preventDefault(); 
+    
     //variables
     var name = $("#name_box").val().trim();
     var destination = $("#destination_box").val().trim();
@@ -25,7 +29,8 @@ $("#submit_button").on("click", function() {
     
     console.log(name, destination, time, frequency);
     
-    //store click data to Firebase
+    // store click extract the data as a single object to hold data, 
+    // push data into the database upon clicking submit,
     database.ref().set( {
         name: name,
         destination: destination,
@@ -35,10 +40,28 @@ $("#submit_button").on("click", function() {
     });
 });
 
-database.ref().on("value")
+//Firebase 
+//on("child_added") to retreive employee data from database and display in the table 
+database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
 
-    //display variables in table 
-    $("#name_box").text()
-    $("#destination_box").text()
-    $("#time_box").text() 
-    $("#frequency_box").text()
+    //display variables in table html
+    var newRow = $("<tr>");
+        newRow.append(
+            "<td>" + snapshot.val().name + "</td>"
+        ).append(
+            "<td>" + snapshot.val().destination + "</td>"
+        ).append(
+            "<td>" + snapshot.val().time + "</td>"
+        ).append(
+            "<td>" + snapshot.val().frequency + "</td>"
+        );
+        $("tbody").append(newRow);
+
+    // $("#name_box").text(snapshot.val().name)
+    // $("#destination_box").text(snapshot.val().destination)     
+    // $("#time_box").text(snapshot.val().time)    
+    // $("#frequency_box").text(snapshot.val().frequency)
+
+}, function (errorObject) {
+    console.log("Error handled: " + errorObject.code);
+});
